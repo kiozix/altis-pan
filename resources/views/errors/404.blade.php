0dfa648b43242cddf940bd6ec626c3d75c9f9683
Lucas GRELAUD <lucas.grelaud@outlook.fr>
2016-01-04 17:14:24 +0100

    Add error for #5

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,20 @@
+@extends('app')
+
+@section('content')
+
+    <aside class="fh5co-page-heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="fh5co-page-heading-lead" style="font-size: 20rem;">
+                        404
+                        <span class="fh5co-border"></span>
+                    </h1>
+                    <p style="font-size: 2rem">Woups, il semblerait que cette page n'existe plus.</p>
+
+                </div>
+            </div>
+        </div>
+    </aside>
+
+@endsection
