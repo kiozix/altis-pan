4bd0365cace259e8f7238f597cf306ccbc3b06c1
Lucas GRELAUD <lucas.grelaud@outlook.fr>
2016-01-09 17:54:42 +0100

    Add style to shop.blade #8

--- resources/views/shops/index.blade.php
+++ resources/views/shops/index.blade.php
@@ -1,7 +1,37 @@
 @extends('app')
 
 @section('content')
+    <aside class="fh5co-page-heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="fh5co-page-heading-lead">
+                        Boutique
+                        <span class="fh5co-border"></span>
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </aside>
+
     <div class="container">
-        <h1>Ceci est test</h1>
+        <div class="row">
+            <div class="col-md-4 panel panel-danger">
+                <div class="panel-body">
+                    <h2 class="fh5co-sidebox-lead text-center">Test</h2>
+                    <hr />
+                    <div class="text-center">
+                      <span class="shop-currency">€</span>
+                      <span class="shop-price">20</span>
+                      <span class="shop-duration">/MO</span>
+                    </div>
+                    <br />
+                    <img class="img-responsive img-rounded" src="http://altislifefr.com/images/uploads/1426543887.jpg">
+                    <br />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus cum ea et eveniet hic impedit maxime, molestias omnis, perspiciatis, praesentium quis ratione repellendus vitae. A dicta maiores quas quia voluptatibus.</p>
+                    <button class="btn btn-outline">Achetter</button>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
