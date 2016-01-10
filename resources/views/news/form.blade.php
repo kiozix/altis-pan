fa824a866a9c1c99165219ef0a6e54df50c6e01c
Lucas GRELAUD <lucas.grelaud@outlook.fr>
2016-01-10 13:31:43 +0100

    Fixe news/form.blade responsive

--- resources/views/news/form.blade.php
+++ resources/views/news/form.blade.php
@@ -7,39 +7,45 @@
 </div>
 
 {!! Form::model($news, ['class' =>'form-horizontal', 'url' => action("NewsController@$action", $news), 'method' => $action == 'store' ? 'Post' : 'Put']) !!}
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group">
+                <label class="col-md-4 control-label">Titre</label>
 
-<div class="form-group">
-    <label class="col-md-4 control-label">Titre</label>
+                <div class="col-md-6">
+                    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                </div>
+            </div>
 
-    <div class="col-md-6">
-        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+            <div class="form-group">
+                <label class="col-md-4 control-label">Slug</label>
+
+                <div class="col-md-6">
+                    {!! Form::text('slug', null, ['class' => 'form-control']) !!}
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-4 control-label">Description</label>
+
+                <div class="col-md-6">
+                    {!! Form::textarea('content', null, ['class' => 'form-control', 'class' => 'ckeditor']) !!}
+                </div>
+            </div>
+
+
+            <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                    <button type="submit" class="btn btn-primary">
+                        Sauvegarder
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
-<div class="form-group">
-    <label class="col-md-4 control-label">Slug</label>
-
-    <div class="col-md-6">
-        {!! Form::text('slug', null, ['class' => 'form-control']) !!}
-    </div>
-</div>
-
-<div class="form-group">
-    <label class="col-md-4 control-label">Description</label>
-
-    <div class="col-md-6">
-        {!! Form::textarea('content', null, ['class' => 'form-control', 'class' => 'ckeditor']) !!}
-    </div>
-</div>
-
-
-<div class="form-group">
-    <div class="col-md-6 col-md-offset-4">
-        <button type="submit" class="btn btn-primary">
-            Sauvegarder
-        </button>
-    </div>
-</div>
 
 {!! Form::close() !!}
 
