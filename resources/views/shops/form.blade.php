60a2533fb1c10dbab40e3e375c1bbb58a161e8e4
Lucas GRELAUD <lucas.grelaud@outlook.fr>
2016-01-10 13:41:10 +0100

    Fixe shop/form.blade responsive

--- resources/views/shops/form.blade.php
+++ resources/views/shops/form.blade.php
@@ -8,53 +8,60 @@
 
 {!! Form::model($shops, ['class' =>'form-horizontal', 'url' => action("ShopsController@$action", $shops), 'method' => $action == 'store' ? 'Post' : 'Put']) !!}
 
-<div class="form-group">
-    <label class="col-md-4 control-label">Titre</label>
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div class="form-group">
+                <label class="col-md-4 control-label">Titre</label>
 
-    <div class="col-md-6">
-        {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                <div class="col-md-6">
+                    {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-4 control-label">Prix (€)</label>
+
+                <div class="col-md-6">
+                    {!! Form::text('price', null, ['class' => 'form-control', 'placeholder' => 'Ex : 10']) !!}
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-4 control-label">Durée (Jours)</label>
+
+                <div class="col-md-6">
+                    {!! Form::text('time', null, ['class' => 'form-control', 'placeholder' => 'Ex : 30']) !!}
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-4 control-label">Image</label>
+
+                <div class="col-md-6">
+                    {!! Form::url('image', null, ['class' => 'form-control', 'placeholder' => 'Ex : http://site.fr/image.jpg']) !!}
+                </div>
+            </div>
+
+            <div class="form-group">
+                <label class="col-md-4 control-label">Description</label>
+
+                <div class="col-md-6">
+                    {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
+                </div>
+            </div>
+
+
+            <div class="form-group">
+                <div class="col-md-6 col-md-offset-4">
+                    <button type="submit" class="btn btn-primary">
+                        Sauvegarder
+                    </button>
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
-<div class="form-group">
-    <label class="col-md-4 control-label">Prix (€)</label>
-
-    <div class="col-md-6">
-        {!! Form::text('price', null, ['class' => 'form-control', 'placeholder' => 'Ex : 10']) !!}
-    </div>
-</div>
-
-<div class="form-group">
-    <label class="col-md-4 control-label">Durée (Jours)</label>
-
-    <div class="col-md-6">
-        {!! Form::text('time', null, ['class' => 'form-control', 'placeholder' => 'Ex : 30']) !!}
-    </div>
-</div>
-
-<div class="form-group">
-    <label class="col-md-4 control-label">Durée (Jours)</label>
-
-    <div class="col-md-6">
-        {!! Form::url('image', null, ['class' => 'form-control', 'placeholder' => 'Ex : http://site.fr/image.jpg']) !!}
-    </div>
-</div>
-
-<div class="form-group">
-    <label class="col-md-4 control-label">Description</label>
-
-    <div class="col-md-6">
-        {!! Form::textarea('content', null, ['class' => 'form-control']) !!}
-    </div>
-</div>
-
-
-<div class="form-group">
-    <div class="col-md-6 col-md-offset-4">
-        <button type="submit" class="btn btn-primary">
-            Sauvegarder
-        </button>
-    </div>
-</div>
 
 {!! Form::close() !!}
