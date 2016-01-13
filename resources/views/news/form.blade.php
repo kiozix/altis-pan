7e6b35c957a3dbcb493833c7739c3410fb2e64a1
Emile LEPETIT <emile.lepetit@gmail.com>
2016-01-13 22:29:13 +0100

    End #12 --Close

--- resources/views/news/form.blade.php
+++ resources/views/news/form.blade.php
@@ -27,13 +27,12 @@
             </div>
 
             <div class="form-group">
-                <label class="col-md-4 control-label">Description</label>
+                <label class="col-md-8 col-md-offset-3 text-left " style="margin-bottom: 15px;">Description</label>
 
-                <div class="col-md-6">
+                <div class="col-md-8 col-md-offset-3">
                     {!! Form::textarea('content', null, ['class' => 'form-control', 'class' => 'ckeditor']) !!}
                 </div>
             </div>
-
 
             <div class="form-group">
                 <div class="col-md-6 col-md-offset-4">
