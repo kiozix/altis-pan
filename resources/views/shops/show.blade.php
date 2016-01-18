f566d9a7997a4ca1b7a5fc0b481b7b615b0904d0
Lucas GRELAUD <lucas.grelaud@outlook.fr>
2016-01-18 19:54:34 +0100

    update shops/show.blade

--- resources/views/shops/show.blade.php
+++ resources/views/shops/show.blade.php
@@ -13,26 +13,38 @@
             </div>
         </div>
     </aside>
-    
+
     <div class="container">
-        <div class="col-md-12">
-            <div class="col-md-6">
-                <img src="{{ $shops->image }}" alt="{{ $shops->name }}" class="img-reponsive img-thumbnail" width="400px" height="400px">
-            </div>
-            <div class="col-md-6">
-                <div class="text-center">
-                    <span class="shop-currency">€</span>
-                    <span class="shop-price">{{ $shops->price }}</span>
-                    @if($shops->time != 0)
-                        <span class="shop-duration">/{{ $shops->time }}Jours</span>
-                    @else
-                        <span class="shop-duration">/A vie</span>
-                    @endif
+        <div class="row">
+            <div class="col-md-12">
+                <div class="col-md-6">
+                    <img src="{{ $shops->image }}" alt="{{ $shops->name }}" class="img-reponsive img-thumbnail" width="400px" height="400px">
                 </div>
-
-                <a href="{{ $paypal }}" class="btn btn-success">Payer</a>
-
-
+                <div class="col-md-6">
+                    <div class="col-md-12">
+                        <div class="text-center">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    <p>{{ $shops->content }}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-12 text-center" style="margin-top: 10px">
+                        <div class="col-md-6">
+                            <a href="{{ $paypal }}" class="btn btn-success">Payer</a>
+                        </div>
+                        <div class="col-md-6">
+                            <span class="shop-currency">€</span>
+                            <span class="shop-price">{{ $shops->price }}</span>
+                            @if($shops->time != 0)
+                                <span class="shop-duration">/{{ $shops->time }} Jours</span>
+                            @else
+                                <span class="shop-duration">/A vie</span>
+                            @endif
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
