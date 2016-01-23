164963effbb103819ee298699d70a8f66e7141d6
Lucas GRELAUD <lucas.grelaud@outlook.fr>
2016-01-23 15:14:55 +0100

    fixe news/index.blade

--- resources/views/news/index.blade.php
+++ resources/views/news/index.blade.php
@@ -24,7 +24,7 @@
                         <div class="fh5co-post-media"><img src="{{ asset('img/news.png') }}" alt="News"></div>
                         <div class="fh5co-post-blurb">
                             <h3 class="text-uppercase">{{ $new->name }}</h3>
-                            {!! substr($new->content,0,20) !!}
+                            {{--{!! substr($new->content,0,20) !!}--}}
                             <span class="fh5co-post-meta">{{ $new->updated_at }}</span>
                         </div>
                     </a>
@@ -33,7 +33,6 @@
                 {!! $news->render() !!}
             </div>
         </div>
-
     </div>
 
 @endsection
