cbaf498d87fdc3c5dc8f65c2be8bc886c2daafbe
Emile LEPETIT <emile.lepetit@gmail.com>
2016-02-04 13:12:24 +0100

    FIX Bugs EmptyLicenses

--- resources/views/admin/players/show.blade.php
+++ resources/views/admin/players/show.blade.php
@@ -473,46 +473,48 @@
                                     <th>Nom</th>
                                     <th>Status</th>
                                 </tr>
-                                <?php
-                                    $suppr = array("\"", "`", "[", "]");
-                                    $lineLicenses = str_replace($suppr, "", $player->civ_licenses);
-                                    $arrayLicenses = preg_split("/,/", $lineLicenses);
-                                    $totarrayLicenses = count($arrayLicenses);
-                                    $y = 0;
-                                    $n = 0;
-                                ?>
-                                @for($i = 1; $y < $totarrayLicenses; $i++)
-                                    <tr>
-                                        <?php
-                                        foreach($licensesName as $license){
-                                            if($arrayLicenses[$y] == $license->value_associated){
-                                                $arrayLicenses[$y]= $license->name;
-                                            }
-                                        }
-                                        ?>
-                                        <td>{{ $arrayLicenses[$y] }}</td>
-                                        <td>
-                                            @if($arrayLicenses[$i] == 1)
-                                                <a href="#" data-group="civ" data-csrf="{{ csrf_token() }}" data-user="{{ $player->playerid }}" data-type="{{ $arrayLicenses[$i] }}" data-id="{{ $n }}" class="licenses-list"><i class="fa fa-check" style="color: #2cc36b;"></i></a>
-                                            @else
-                                                <a href="#" data-group="civ" data-csrf="{{ csrf_token() }}" data-user="{{ $player->playerid }}" data-type="{{ $arrayLicenses[$i] }}" data-id="{{ $n }}" class="licenses-list"><i class="fa fa-close" style="color: #c0392b;"></i></a>
-                                            @endif
-                                        </td>
-                                    </tr>
-
+                                @if($player->civ_licenses)
                                     <?php
-                                    $y = $y + 2;
-                                    $i = $i + 1;
-                                    $n = $n + 1;
+                                        $suppr = array("\"", "`", "[", "]");
+                                        $lineLicenses = str_replace($suppr, "", $player->civ_licenses);
+                                        $arrayLicenses = preg_split("/,/", $lineLicenses);
+                                        $totarrayLicenses = count($arrayLicenses);
+                                        $y = 0;
+                                        $n = 0;
                                     ?>
-                                @endfor
+                                    @for($i = 1; $y < $totarrayLicenses; $i++)
+                                        <tr>
+                                            <?php
+                                            foreach($licensesName as $license){
+                                                if($arrayLicenses[$y] == $license->value_associated){
+                                                    $arrayLicenses[$y]= $license->name;
+                                                }
+                                            }
+                                            ?>
+                                            <td>{{ $arrayLicenses[$y] }}</td>
+                                            <td>
+                                                @if($arrayLicenses[$i] == 1)
+                                                    <a href="#" data-group="civ" data-csrf="{{ csrf_token() }}" data-user="{{ $player->playerid }}" data-type="{{ $arrayLicenses[$i] }}" data-id="{{ $n }}" class="licenses-list"><i class="fa fa-check" style="color: #2cc36b;"></i></a>
+                                                @else
+                                                    <a href="#" data-group="civ" data-csrf="{{ csrf_token() }}" data-user="{{ $player->playerid }}" data-type="{{ $arrayLicenses[$i] }}" data-id="{{ $n }}" class="licenses-list"><i class="fa fa-close" style="color: #c0392b;"></i></a>
+                                                @endif
+                                            </td>
+                                        </tr>
+
+                                        <?php
+                                        $y = $y + 2;
+                                        $i = $i + 1;
+                                        $n = $n + 1;
+                                        ?>
+                                    @endfor
+                                @endif
                             </table>
 
                         </div>
                     </div>
                 </div>
 
-                @if($player->coplevel >= 1)
+                @if($player->coplevel >= 1 && $player->cop_licenses)
                 <div id="licenses-cop" class="panel panel-default">
                     <div class="panel-heading">
                         <span style="font-weight: bold;font-size: 20px !important;">Licences policier</span>
@@ -564,7 +566,7 @@
                 </div>
                 @endif
 
-                @if($player->mediclevel >= 1)
+                @if($player->mediclevel >= 1 && $player->med_licenses)
                 <div id="licenses-med" class="panel panel-default">
                     <div class="panel-heading">
                         <span style="font-weight: bold;font-size: 20px !important;">Licences pompier</span>
